b0e26fdd451ca1743d924d08ce0d56d8adc64a73
Ansel Blume <anselblume@gmail.com>
2021-04-13 00:46:26 -0700

    Initial commit

---
+++ src/encode_state.py
@@ -0,0 +1,78 @@
+import torch
+import chess
+# Pages 12-15 of paper https://arxiv.org/pdf/1712.01815.pdf are most relevant
+N = 8 # Board dimensions
+M = 14 # 6 p1 pieces, 6 p2 pieces, 2 repetition counts
+L = 7
+
+class StateEncoder:
+    '''
+        Dimensions N x N x (MT + L)
+
+        N x N
+        a8 -> (0, 0), a0 -> (7, 0), h8 -> (0, 7), h0 -> (7, 7)
+
+        M array follows values of chess lib's piece types offset by one
+        M: P, N, B, R, Q, K, p, n, b, r, q, k, Rep, rep
+
+        L: 1 player color, 1 total move count, 2 p1 castling, 2 p2 castling, 1 no-progress count
+    '''
+    def __init__(self, T=2):
+        self.T = T # Length of history
+
+    def piece_to_index(self, piece, board):
+        '''
+            Returns index of the piece in the M array (not accounting for T offset).
+        '''
+        # Offset for player or opponent's piece
+        offset = 0 if piece.color == board.turn else 6
+        return offset + piece.piece_type - 1
+
+    def square_to_n_n(self, square):
+        '''
+            Returns the indices in the N x N board corresponding to square.
+        '''
+        rank = chess.square_rank(square)
+        file = chess.square_file(square)
+
+        return N - (rank + 1), file
+
+    def encode_state(self, board, prev_state):
+        '''
+            Returns the input tensor to be processed by AlphaZero.
+        '''
+        T = self.T
+        state = torch.zeros(M*T+L, N, N)
+
+        # Encode history, dropping the oldest state
+        state[:M*(T-1), ...] = prev_state[M:M*T,...]
+
+        # Encode pieces
+        t_offset = M*(T-1)
+        for square, piece in board.piece_map().items():
+            piece_ind = self.piece_to_index(piece, board)
+            row, col = self.square_to_n_n(square)
+            state[t_offset + piece_ind, row, col] = 1
+
+        # TODO Potentially encode repeat count for both players
+
+        # Encode current player color
+        state[M*T, ...] += board.turn
+
+        # Encode total move count
+        state[M*T + 1, ...] += len(board.move_stack)
+
+        # Encode player castling
+        state[M*T + 2, ...] += board.has_queenside_castling_rights(board.turn)
+        state[M*T + 3, ...] += board.has_kingside_castling_rights(board.turn)
+
+        # Encode opponent castling
+        state[M*T + 4, ...] += board.has_queenside_castling_rights(not board.turn)
+        state[M*T + 5, ...] += board.has_kingside_castling_rights(not board.turn)
+
+        # TODO Potentially encode progress count
+
+        return state
+
+    def get_empty_state(self):
+        return torch.zeros(M*(self.T)+L, N, N)
---
+++ test/test_state.py
@@ -0,0 +1,130 @@
+import unittest
+import os
+import sys
+sys.path.insert(1, os.path.realpath('../src'))
+import encode_state
+from encode_state import StateEncoder
+import chess
+import torch
+
+class TestState(unittest.TestCase):
+    def setUp(self):
+        self.encoder = StateEncoder(T=2)
+
+    def test_piece_to_index(self):
+        # Test 1
+        board = chess.Board() # color is True by default
+        piece = chess.Piece(chess.ROOK, False)
+        index = self.encoder.piece_to_index(piece, board)
+        expected = 9
+
+        self.assertEqual(index, expected)
+
+        # Test 2
+        board = chess.Board()
+        piece = chess.Piece(chess.QUEEN, True)
+        index = self.encoder.piece_to_index(piece, board)
+        expected = 4
+
+        self.assertEqual(index, expected)
+
+        # Test 3
+        board = chess.Board()
+        board.turn = False
+        piece = chess.Piece(chess.PAWN, True)
+        index = self.encoder.piece_to_index(piece, board)
+        expected = 6
+
+        self.assertEqual(index, expected)
+
+    def test_square_to_n_n(self):
+        # Test 1
+        square = chess.parse_square('a1')
+        n_n = self.encoder.square_to_n_n(square)
+        expected = (7, 0)
+
+        self.assertEqual(n_n, expected)
+
+        # Test 2
+        square = chess.parse_square('g3')
+        n_n = self.encoder.square_to_n_n(square)
+        expected = (5, 6)
+
+        self.assertEqual(n_n, expected)
+
+    def test_encode_state(self):
+        M, N = encode_state.M, encode_state.N
+        T = self.encoder.T
+
+        # Test 1 (https://lichess.org/editor/8/8/8/1k6/2n5/5K2/2B5/8_w_-_-_0_1)
+        board = chess.Board(fen='8/8/8/1k6/2n5/5K2/2B5/8 w - - 0 1')
+        prev_state = self.encoder.get_empty_state()
+        state = self.encoder.encode_state(board, prev_state)
+
+        # Check history
+        expected_history = 0
+        history = state[:M*(T-1),...]
+        self.assertTrue((history == expected_history).all())
+
+        # Check position planes
+        expected_white_pos = torch.zeros(6, N, N)
+        expected_white_pos[2, 6, 2] = 1 # Bishop
+        expected_white_pos[5, 5, 5] = 1 # King
+
+        expected_black_pos = torch.zeros(6, N, N)
+        expected_black_pos[5, 3, 1] = 1 # King
+        expected_black_pos[1, 4, 2] = 1 # Knight
+
+        player_pos = state[M*(T-1):M*(T-1)+6, ...]
+        opponent_pos = state[M*(T-1)+6:M*(T-1)+12, ...]
+
+        self.assertTrue((player_pos == expected_white_pos).all())
+        self.assertTrue((opponent_pos == expected_black_pos).all())
+
+        # Check L planes
+        player_color = state[M*T, ...]
+        expected_color = 1
+        self.assertTrue((player_color == expected_color).all())
+
+        move_count = state[M*T+1, ...]
+        expected_count = 0
+        self.assertTrue((move_count == expected_count).all())
+
+        player_castling = state[M*T+2:M*T+4, ...]
+        expected_castling = 0
+        self.assertTrue((player_castling == expected_castling).all())
+
+        opponent_castling = state[M*T+4:M*T+6, ...]
+        expected_castling = 0
+        self.assertTrue((opponent_castling == expected_castling).all())
+
+        # Take a move (https://lichess.org/editor/8/8/8/1k6/2n5/8/2B1K3/8_w_-_-_0_1)
+        ke2 = chess.Move.from_uci('f3e2')
+        board.push(ke2)
+        prev_state = state
+        state = self.encoder.encode_state(board, prev_state)
+
+        # Check history
+        history = state[:M*(T-1), ...]
+        expected_history = prev_state[M:M*T, ...]
+        self.assertTrue((history == expected_history).all())
+
+        # Check positions
+        expected_white_pos[5,...] = 0 # White king moved; need to adjust expected
+        expected_white_pos[5, 6, 4] = 1
+
+        player_pos = state[M*(T-1):M*(T-1)+6, ...]
+        opponent_pos = state[M*(T-1)+6:M*(T-1)+12,...]
+
+        self.assertTrue((player_pos == expected_black_pos).all()) # Reverse of before
+        self.assertTrue((opponent_pos == expected_white_pos).all())
+
+        # Check current player color
+        player_color = state[M*T, ...]
+        expected_color = 0
+        self.assertTrue((player_color == expected_color).all())
+
+        # Check move count
+        move_count = state[M*T+1, ...]
+        expected_count = 1
+        self.assertTrue((move_count == expected_count).all())
