Add Dirichlet noise; fix analyze.py's MCTS computation

src/mcts/tree.py
@@ -1,5 +1,6 @@
 import chess
 import numpy as np
+from numpy.random import dirichlet
 import logging
 
 logger = logging.getLogger(__name__)
@@ -65,7 +66,12 @@ class TreeNode:
 
         # For each action, create a child node
         board = chess.Board(self.fen)
-        for i, move in enumerate(board.legal_moves):
+        legal_moves = list(board.legal_moves)
+
+        # Dirichlet noise with concentration .3 as specified in AlphaZero
+        noise = dirichlet(np.zeros(len(legal_moves)) + .3)
+
+        for i, move in enumerate(legal_moves):
             # Compute the next state from the move
             board.push(move)
             child_fen = board.fen()
@@ -78,7 +84,7 @@ class TreeNode:
                 child_state = TreeNode(child_fen, self.fen_to_node, is_rollout=is_rollout)
 
             # Add an edge to the new state from this node
-            prior = prior_func(move)
+            prior = .75 * prior_func(move) + .25 * noise[i] # Add Dirichlet noise with mixing as in AlphaGo Zero
             out_edge = TreeEdge(
                 move.uci(),
                 prior,

src/run/analyze.py
@@ -28,8 +28,8 @@ def top_net_moves(fen, network, T=8, temp=2, k=5, device='cpu'):
 
     return val, list(zip(top_ucis, top_probs))
 
-def top_mcts_moves(fen, network, T=8, temp=2, k=5, device='cpu',
-                   max_trials=1000, max_time_s=10):
+def top_mcts_moves(fen, network, T=8, temp=1, k=5, device='cpu',
+                   max_trials=1000, max_time_s=5):
     # Build inputs for MCTS
     state_encoder = StateEncoder(T)
 
@@ -45,7 +45,7 @@ def top_mcts_moves(fen, network, T=8, temp=2, k=5, device='cpu',
         curr_state = state_encoder.encode_state_with_history(boards)
         with torch.no_grad():
             curr_state = curr_state.unsqueeze(0).to(device)
-            _, net_log_probs = network(curr_state)
+            value, net_log_probs = network(curr_state)
 
         net_policy = net_log_probs.exp().squeeze()
         mask_invalid_moves(net_policy, chess.Board(fens[-1]), device=device)
@@ -55,7 +55,7 @@ def top_mcts_moves(fen, network, T=8, temp=2, k=5, device='cpu',
             index = square_move_to_index(move.from_square, move.to_square, move.promotion)
             return net_policy[row, col, index]
 
-        return prior_func
+        return value, net_policy, prior_func
 
     # Run MCTS
     mcts_evaluator = MCTSEvaluator(fen, prior_func_builder)
@@ -63,8 +63,8 @@ def top_mcts_moves(fen, network, T=8, temp=2, k=5, device='cpu',
 
     # Get move distribution; computation analogous to GameRunner._sample_move
     # All moves are legal so no need to remove probability 0 moves like in top_net_moves
-    visits = torch.tensor([edge.n_visits for edge in root.out_edges])
-    visits = F.softmax(visits / temp, dim=0)
+    visits = torch.tensor([edge.n_visits for edge in root.out_edges]).float()
+    visits /= visits.sum()
 
     top_probs, top_indices = torch.topk(visits, k)
     top_probs = (top_prob.item() for top_prob in top_probs)

src/run/play.py
@@ -123,7 +123,7 @@ class GameRunner:
         return MCTSDist(root, effective_temp), root.out_edges[sampled_ind].to_node
 
     def _sample_move(self, root, effective_temp):
-        visits = torch.tensor([edge.n_visits for edge in root.out_edges])
+        visits = torch.tensor([edge.n_visits for edge in root.out_edges]).float()
 
         # In the same way as in MCTSPolicyEncoder, perform softmax over visits
         # as opposed to this harder version used in the original paper